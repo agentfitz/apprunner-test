e2ecb735ee5b5dec1a2f3643900d69980fe853fe
Brian FitzGerald <bmfitzgerald3@gmail.com>
2022-10-31 21:23:42 -0400

    cute quote

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let server = http.createServer((request, response) => {
     if (request.url === '/about') {
         response.write('<h1>About us.</h1>');
     } else if (request.url === '/monkeys') {
-        response.write('<h1>Monkeys are great.</h1>');
+        response.write('<h1>Monkeys are great. You are a cute monkey.</h1>');
     } else {
         response.write('<h1>Hello World.</h1>');
     }
